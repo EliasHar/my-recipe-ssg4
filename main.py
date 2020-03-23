db8d65eb0f11a6bc768f03d0b79b34b506240435
EliasHar <54663653+EliasHar@users.noreply.github.com>
2020-03-23 21:06:59 +0000

    Laga layout

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from jinja2 import Environment, PackageLoader
 from markdown2 import markdown
 
-
 POSTS = {}
 
 for markdown_post in os.listdir('content'):
@@ -13,7 +12,6 @@ for markdown_post in os.listdir('content'):
     with open(file_path, 'r') as file:
         POSTS[markdown_post] = markdown(file.read(), extras=['metadata'])
 
-    
 POSTS = {
     post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
 }
@@ -23,12 +21,13 @@ index_template = env.get_template('index.html')
 bread_template = env.get_template('bread.html')
 post_template = env.get_template('post.html')
 
+# forsíðan er ekki með MD post renderingu
 index_html = index_template.render()
 
+# brauðuppskriftir
 posts_metadata = [POSTS[post].metadata for post in POSTS]
 tags = [post['tags'] for post in posts_metadata]
 bread_html = bread_template.render(posts=posts_metadata, tags=tags)
-
 
 with open('../my-recipes4/index.html', 'w', encoding='utf-8') as file:
     file.write(index_html)
@@ -43,7 +42,7 @@ for post in POSTS:
         'content': POSTS[post],
         'title': post_metadata['title'],
         'date': post_metadata['date'],
-        'thumbnail':  post_metadata['thumbnail']
+        'thumbnail': post_metadata['thumbnail']
     }
 
     post_html = post_template.render(post=post_data)
@@ -52,3 +51,4 @@ for post in POSTS:
     os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
     with open(post_file_path, 'w') as file:
         file.write(post_html)
+
